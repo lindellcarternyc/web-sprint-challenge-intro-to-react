e057f04d3ed35b829c29e1f2f5bef9c6ba3a4b5f
Lindell Carter <lindellcarternyc@gmail.com>
2021-02-19 08:49:26 -0500

    initial file cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -16,4 +16,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App
